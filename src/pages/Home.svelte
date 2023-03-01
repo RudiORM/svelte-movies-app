216dfebc33039e720134a05f187ecc6b16cc7d4c
RudiORM <53378885+RudiORM@users.noreply.github.com>
2023-03-01 18:33:01 +0000

    putting in dummy API key

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,6 +2,8 @@
 	import MainSection from '../components/MainSection.svelte'
 
 	const ApiKey = __myapp.env.API_KEY
+	console.log('my fake key is:')
+	console.log(ApiKey)
 	const FEATURED_API = `https://api.themoviedb.org/3/trending/movie/week?api_key=${ApiKey}&language=en-GB`
 
 	let api_url = (FEATURED_API + '&page=')
